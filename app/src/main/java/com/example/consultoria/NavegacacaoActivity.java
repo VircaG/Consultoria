50650606dfbbf4baf4e47591c9edb52e2b1bf023
Virca Gonçalves <vircagoncalves39@gmail.com>
2021-04-28 23:32:34 +0100

    Adição de funcionalidade ennviar Email dentro da app

--- app/src/main/java/com/example/consultoria/NavegacacaoActivity.java
+++ app/src/main/java/com/example/consultoria/NavegacacaoActivity.java
@@ -1,5 +1,7 @@
 package com.example.consultoria;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -30,8 +32,7 @@ public class NavegacacaoActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                enviarEmail();
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -48,6 +49,24 @@ public class NavegacacaoActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
+        public void enviarEmail(){
+        String telefone = "tel:923567864";
+        String image ="https://epocanegocios.globo.com/Empresa/noticia/2020/09/para-atrair-clientes-resort-nas-maldivas-lanca-pacote-de-home-office-de-luxo-por-us-23-mil.html";
+        String endereco ="https://www.google.com/maps?q=colombo+lisboa&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjei6jV_KHwAhXJ6eAKHQ2FCbUQ_AUoAXoECAEQAw";
+            // Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(telefone));
+            //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(imagem);
+            // Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(image));
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+
+            intent.putExtra(Intent.EXTRA_EMAIL,new String []{"atendimento@atmconsultoria.pt"});
+            intent.putExtra(Intent.EXTRA_SUBJECT,"Contato pelo App");
+            intent.putExtra(Intent.EXTRA_TEXT,"Mensagem automática");
+
+            intent.setType("mensagem/rfc822");
+
+            startActivity(Intent.createChooser(intent,"Compartilhar"));
+        }
 
 
     @Override
